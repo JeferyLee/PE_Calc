00320f9785092db36e3077b808c8c75154796a46
JeferyLee <lixing.wo1990@qq.com>
2019-01-25 17:37:59 +0800

    add filemenu function

--- PE_GUI.py
+++ PE_GUI.py
@@ -1,34 +1,50 @@
 import tkinter as tk
-from tkinter import *
+
 from PIL import ImageTk,Image
+from tkinter import Menu
+from tkinter import messagebox
+from tkinter import ttk             #ttk为tk的一种简单升级
+
+
 window=tk.Tk()
-window.geometry('1200x880')
+window.geometry('900x700')
 window.title('for PE_calc')
 
+
+window.update()
+
 #设置画布的参数，长宽和背景颜色
-canvas1=tk.Canvas(window,height=1080,width=1920)
-# img_file=tk.PhotoImage(file='2.gif')
+cav_width=window.winfo_width()
+cav_height=window.winfo_height()
+#根据窗口的长宽改变画布长宽
+canvas1=tk.Canvas(window,height=cav_height-100,width=cav_width)
+
+
 img=Image.open('2.gif')
 photo=ImageTk.PhotoImage(img)
-#这里的坐标指的是图片的中心在画布中的位置
+#这里的坐标指的是图片的中心在画布中的位置,这里的参数第一个是宽度
 
-#这里的参数第一个是宽度
-canvas1.create_image(960,540,image=photo)
-# canvas1.pack(fill=BOTH)
+canvas1.create_image(cav_width*0.5,cav_height*0.5,image=photo)
 canvas1.pack()
 
-#为一个容器
-menubar=tk.Menu(window)
-filemenu=tk.Menu(menubar,tearoff=0)
+#menubar为一个容器,增加菜单栏功能
+menubar=Menu(window)
+window.config(menu=menubar)
+
+#创建file菜单栏
+filemenu=Menu(menubar,tearoff=0)
 menubar.add_cascade(label='File',menu=filemenu)
 def do_job():
     pass
-filemenu.add_command(label='New', command=do_job)
-filemenu.add_command(label='Open', command=do_job)##同样的在`File`中加入`Open`小菜单
-filemenu.add_command(label='Save', command=do_job)##同样的在`File`中加入`Save`小菜单
-filemenu.add_separator()##这里就是一条分割线
 
-##同样的在`File`中加入`Exit`小菜单,此处对应命令为`window.quit`
-filemenu.add_command(label='Exit', command=window.quit)
+filemenu.add_command(label='New', command=do_job)
+filemenu.add_command(label='open',command=do_job)
+filemenu.add_separator()
+filemenu.add_command(label='exit',command=window.quit)
+
+def hit_me():
+    tk.messagebox.showinfo(title='Hi',message='haha')
+btn1=ttk.Button(window,text='hit me',command=hit_me).pack()
+window.iconbitmap('car.ico')
 
 window.mainloop()
